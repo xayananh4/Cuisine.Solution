b5b2265e18b5284eba6138d28fe83bec3edd1525
xayananh4@gmail.com <viradeth xay-ananh>
2023-03-09 11:07:20 -0800

    add dropdown box with listed cuisine on homepage, user is taken to restaurant associated with that cuisine
    Co-Authored-By:Brent Hickman <brenthickman@ymail.com>

--- CuisineTracker/Controllers/HomeController.cs
+++ CuisineTracker/Controllers/HomeController.cs
@@ -1,15 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CuisineTracker.Models;
+
+
 
 namespace CuisineTracker.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly CuisineContext _db;
+
+    public HomeController(CuisineContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Description");
+      return View();
+    }
+
+
+    [HttpPost]
+    public ActionResult Index(int CuisineId)
+    {
+      return RedirectToAction("Details","Cuisines", new { id = CuisineId });
+    }
+
+
+
+  }
 }
